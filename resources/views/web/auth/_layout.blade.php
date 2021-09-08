c291f853507a6de48a61287d45d37e018d22bf8b
Marijn van Wezel <marijnvanwezel@gmail.com>
2021-09-08 22:01:34 +0200

    Add navbar to 'auth' layout

--- resources/views/web/auth/_layout.blade.php
+++ resources/views/web/auth/_layout.blade.php
@@ -1,6 +1,11 @@
 @extends('web._layout')
 
 @section('body')
+    <div class="uk-navbar-container uk-navbar-transparent" uk-navbar="uk-navbar">
+        <div class="uk-navbar-center">
+            <a href="{{ route('index') }}" class="uk-navbar-item uk-logo"></a>
+        </div>
+    </div>
     <div id="app">
         @yield('app')
     </div>
